d7503b81df56a15044e02d91b7a6b15bbba2a701
itsvagapov <m.vagapovv.11@gmail.com>
2026-04-30 14:38:19 +0300

    json unmarshal

--- intocode/main.go
+++ intocode/main.go
@@ -27,17 +27,50 @@ type WorkDay struct {
     Mentors     []string `json:"mentors"`
 }
 
+type Student struct {
+    FirstName string `json:"first_name"`
+    LastName  string `json:"last_name"`
+    Age       int    `json:"age"`
+    City      string `json:"city"`
+    WantsGo   bool   `json:"wants_go"`
+}
+
 func main() {
-	w := WorkDay{
-		Title: "Первый день в Intocode",
-		City: "Грозный",
-		Address: "ул. Дадин Айбики",
-		IsOnline: false,
-		Seats: 20,
-		Mentors: []string{"Кудузов Ахмад", "Назиров Расул"},
+	studentJSON := `{
+  "first_name": "Иса",
+  "last_name": "Исаев",
+  "age": 17,
+  "city": "Грозный",
+  "wants_go": true
+}`
+
+	var s Student
+
+	// fmt.Println(studentJSON)
+
+	if err := json.Unmarshal([]byte(studentJSON), &s); err != nil {
+		fmt.Println("Ошибка JSON:", err)
 	}
 
-	jsonBytes, _ := json.MarshalIndent(w, "", "\t")
+	if s.WantsGo && s.Age >= 16 {
+		fmt.Printf("%s %s готов к Буткемпу Intocode в Грозном!\n", s.FirstName, s.LastName)
+	} else if s.Age < 16 {
+		fmt.Println("слишком маленький возраст!")
+	}
+	
 
-	fmt.Println(string(jsonBytes))
+	// fmt.Println(s)
+
+	// w := WorkDay{
+		// Title: "Первый день в Intocode",
+		// City: "Грозный",
+		// Address: "ул. Дадин Айбики",
+		// IsOnline: false,
+		// Seats: 20,
+		// Mentors: []string{"Кудузов Ахмад", "Назиров Расул"},
+	// }
+
+	// jsonBytes, _ := json.MarshalIndent(w, "", "\t")
+
+	// fmt.Println(string(jsonBytes))
 }
